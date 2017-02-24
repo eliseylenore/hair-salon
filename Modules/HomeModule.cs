6da9116d997f8302b32985c30e2cd408f12d9e03
Elise St Hilaire <eliseylenore@gmail.com>
2017-02-24 12:58:39 -0800

    Add views for stylists, clients, and adding a new stylist

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,6 +12,26 @@ namespace HairSalon
             Get["/"] = _ => {
                 return View["index.cshtml"];
             };
+
+            Get["/clients"] = _ => {
+                List<Client> AllClients = Client.GetAll();
+                return View["clients.cshtml", AllClients];
+            };
+
+            Get["/stylists"] = _ => {
+                List<Stylist> AllStylists = Stylist.GetAll();
+                return View["stylists.cshtml", AllStylists];
+            };
+
+            Get["/stylist/new"] = _ => {
+                return View["stylist_new.cshtml"];
+            };
+
+            Post["/stylists"] = _ => {
+                Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
+                newStylist.Save();
+                return View["stylists.cshtml"];
+            };
         }
     }
 }
