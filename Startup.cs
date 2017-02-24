2d65d8e1558fdfce2910be45f20d49143bff83d2
Elise St Hilaire <eliseylenore@gmail.com>
2017-02-24 08:25:52 -0800

    Set up project.json to include System.Data, configure application to connect to database, add README

--- Startup.cs
+++ Startup.cs
@@ -39,4 +39,9 @@ namespace Namespace
       get { return false; }
     }
   }
+  
+  public static class DBConfiguration
+    {
+        public static string ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo;Integrated Security=SSPI;";
+    }
 }
