8b42a37fb6df0e8e8700459fd5195e982553edd6
Elise St Hilaire <eliseylenore@gmail.com>
2017-02-24 14:33:01 -0800

    Add route to delete stylist

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -18,6 +18,19 @@ namespace HairSalon
                 return View["clients.cshtml", AllClients];
             };
 
+            Get["/client/new"] = _ => {
+                List<Stylist> AllStylists = Stylist.GetAll();
+                return View["client_new.cshtml", AllStylists];
+            };
+
+            Post["/clients"] = _ => {
+                Client newClient = new Client(Request.Form["client-name"], Request.Form["stylist-id"]);
+                newClient.Save();
+                List<Client> AllClients = Client.GetAll();
+                return View["clients.cshtml", AllClients];
+            };
+
+
             Get["/stylists"] = _ => {
                 List<Stylist> AllStylists = Stylist.GetAll();
                 return View["stylists.cshtml", AllStylists];
@@ -34,18 +47,18 @@ namespace HairSalon
                 return View["stylists.cshtml", AllStylists];
             };
 
+            Get["stylist/delete/{id}"] = parameters => {
+                Stylist SelectedStylist = Stylist.Find(parameters.id);
+                return View["stylist_delete.cshtml", SelectedStylist];
+            };
 
-            Get["/client/new"] = _ => {
+            Delete["stylist/delete/{id}"] = parameters => {
+                Stylist SelectedStylist = Stylist.Find(parameters.id);
+                SelectedStylist.Delete();
                 List<Stylist> AllStylists = Stylist.GetAll();
-                return View["client_new.cshtml", AllStylists];
+                return View["stylists.cshtml", AllStylists];
             };
 
-            Post["/clients"] = _ => {
-                Client newClient = new Client(Request.Form["client-name"], Request.Form["stylist-id"]);
-                newClient.Save();
-                List<Client> AllClients = Client.GetAll();
-                return View["clients.cshtml", AllClients];
-            };
         }
     }
 }
