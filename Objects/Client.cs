b7f26375fa77df30ecfb71a5f1e6d3d39de8f86a
Elise St Hilaire <eliseylenore@gmail.com>
2017-02-24 09:36:52 -0800

    Test that database is empty at first

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -1,9 +1,70 @@
-using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
 
- namespace HairSalon.Objects
- {
-     public class Class
-     {
-     }
- }
+namespace HairSalon.Objects
+{
+    public class Client
+    {
+        private int _id;
+        private string _name;
+
+        public Client(string Name, int Id = 0)
+        {
+            _id = Id;
+            _name = Name;
+        }
+
+        public int GetID()
+        {
+            return _id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+        public static List<Client> GetAll()
+        {
+            List<Client> allClients = new List<Client>{};
+
+            // SqlConnection conn = DB.Connection();
+            // conn.Open();
+            //
+            // SqlCommand cmd = new SqlCommand("SELECT * FROM clients;", conn);
+            // SqlDataReader rdr = cmd.ExecuteReader();
+            //
+            // while(rdr.Read())
+            // {
+            //     int clientId = rdr.GetInt32(0);
+            //     string clientName = rdr.GetString(1);
+            //     Client newClient = new Client(clientName, clientId);
+            //     allClients.Add(newClient);
+            // }
+            //
+            // if (rdr != null)
+            // {
+            //     rdr.Close();
+            // }
+            // if (conn != null)
+            // {
+            //     conn.Close();
+            // }
+
+            return allClients;
+        }
+        public static void DeleteAll()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM clients;", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
--- Tests/ClientTest.cs
+++ Tests/ClientTest.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System;
 using System.Data;
 using System.Data.SqlClient;
+using HairSalon.Objects;
 
 namespace HairSalon
 {
-  public class ClientTest : IDisposable
-  {
-    public HairSalonTest()
+    public class ClientTest : IDisposable
     {
-      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
-    }
+        public void HairSalonTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+        }
 
-    public void Dispose()
-    {
-      Task.DeleteAll();
+        [Fact]
+        public void Test_DatabaseEmptyAtFirst()
+        {
+            //Arrange, Act
+            int result = Client.GetAll().Count;
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        public void Dispose()
+        {
+            Client.DeleteAll();
+        }
     }
-  }
 }
