31c215dcc7edf6a6dc6c11f311392b7dce3495cf
Elise St Hilaire <eliseylenore@gmail.com>
2017-02-24 12:15:41 -0800

    Add test for delete method and fail

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -118,6 +118,36 @@ namespace HairSalon
       Assert.Equal(newName, result);
     }
 
+    [Fact]
+    public void Test_Delete_DeletesStylistFromDatabase()
+    {
+      //Arrange
+      string name1 = "Betsy DeVoss";
+      Stylist testStylist1 = new Stylist(name1);
+      testStylist1.Save();
+
+      string name2 = "Cherie Miller";
+      Stylist testStylist2 = new Stylist(name2);
+      testStylist2.Save();
+
+      Client testClient1 = new Client("Betsy Ross", testStylist1.GetId());
+      testClient1.Save();
+      Client testClient2 = new Client("Cherie St Hilaire", testStylist2.GetId());
+      testClient2.Save();
+
+      //Act
+      testStylist1.Delete();
+      List<Stylist> resultStylists = Stylist.GetAll();
+      List<Stylist> testStylistList = new List<Stylist> {testStylist2};
+
+      List<Client> resultClients = Client.GetAll();
+      List<Client> testClientList = new List<Client> {testClient2};
+
+      //Assert
+      Assert.Equal(testStylistList, resultStylists);
+      Assert.Equal(testClientList, resultClients);
+    }
+
     public void Dispose()
     {
       Client.DeleteAll();
